21cbff63bae79b41b6d945bbd58ef42387a95213
Cleidson Medeiros <cleidsonmedeiros14@gmail.com>
2020-01-23 23:37:00 -0300

    implement ambient_rails helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
 	def data_br(data_us)
 		data_us.strftime("%d/%m/%Y")
 	end
+
+	def ambient_rails
+		if Rails.env.development?
+			"Development"
+		elsif Rails.env.production?
+			"Production"
+		else
+			"Test"
+		end
+	end
 end
+
